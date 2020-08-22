b5c9bcd86e2c9b182cb49821df13add6cfff3050
ravi@raviojha <raviojha477@gmail.com>
2020-08-22 13:22:27 -0700

    Updates

---
+++ precache-manifest.e2d31b7e9fe85492e5cac28440370876.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f211e3f0b8536d0f954143c1e2f9301b",
+    "url": "/RaviOjha-Protfolio/index.html"
+  },
+  {
+    "revision": "caf428a6629b4ebb32cc",
+    "url": "/RaviOjha-Protfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "d35ed393e1b425fecc96",
+    "url": "/RaviOjha-Protfolio/static/css/main.354f2162.chunk.css"
+  },
+  {
+    "revision": "caf428a6629b4ebb32cc",
+    "url": "/RaviOjha-Protfolio/static/js/2.00188906.chunk.js"
+  },
+  {
+    "revision": "7b16521b2b214bd3cdc23244c1721340",
+    "url": "/RaviOjha-Protfolio/static/js/2.00188906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d35ed393e1b425fecc96",
+    "url": "/RaviOjha-Protfolio/static/js/main.c71ca206.chunk.js"
+  },
+  {
+    "revision": "5eafd59896a3316a30c5",
+    "url": "/RaviOjha-Protfolio/static/js/runtime-main.91d986fd.js"
+  }
+]);
